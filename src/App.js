d646ef52f36723b3567e87f14af9091ba458f066
SATYAM CHATTERJEE <satyamchatterjee@192.168.1.2>
2024-04-13 16:49:45 +0530

    feat(switch-case): add a react implementation of switch case

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Children } from "react";
+
+function SwitchComponent({ children, value }) {
+  let cases = [];
+  let defaults = [];
+
+  Children.forEach(children, (e) => {
+    if (e.type.name === "CaseComponent") {
+      if (typeof e.props.value === "function" && e.props.value(value)) {
+        cases.push(e);
+      } else if (e.props.value === value) {
+        cases.push(e);
+      }
+    } else if (e.type.name === "DefaultComponent") {
+      defaults.push(e);
+    }
+  });
+
+  return cases.length > 0 ? cases : defaults;
+}
+
+function CaseComponent({ children }) {
+  return <>{children}</>;
+}
+
+function DefaultComponent({ children }) {
+  return <>{children}</>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SwitchComponent value="36">
+      <CaseComponent value={(e) => e > 20}>
+        <h1>Function hun main</h1>
+      </CaseComponent>
+      <CaseComponent value="10">
+        <h1>10 is the value</h1>
+      </CaseComponent>
+      <CaseComponent value="20">
+        <h1>20 is the value</h1>
+      </CaseComponent>
+      <CaseComponent value="30">
+        <h1>30 is the value</h1>
+      </CaseComponent>
+      <CaseComponent value="40">
+        <h1>40 is the value</h1>
+      </CaseComponent>
+      <DefaultComponent>
+        <h1>Default hun main</h1>
+      </DefaultComponent>
+    </SwitchComponent>
   );
 }
 
